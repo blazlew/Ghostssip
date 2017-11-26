ca62304cb105248270f292be78b991c06e7c175c
ledsoon <ledek89@gmail.com>
2017-11-26 09:37:16 +0100

    Bottom menu added, small fixes

--- app/src/main/java/com/example/ledsoon/ghostssip/MainActivity.java
+++ app/src/main/java/com/example/ledsoon/ghostssip/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.ledsoon.ghostssip;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,11 +13,15 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.crowdfire.cfalertdialog.CFAlertDialog;
+import com.luseen.spacenavigation.SpaceItem;
+import com.luseen.spacenavigation.SpaceNavigationView;
+import com.luseen.spacenavigation.SpaceOnClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView title;
     private EditText newName;
+    private SpaceNavigationView bottomMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +30,34 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.title_layout);
         title = getSupportActionBar().getCustomView().findViewById(R.id.title);
+        bottomMenu = findViewById(R.id.bottomMenu);
+        bottomMenu.initWithSaveInstanceState(savedInstanceState);
+        setUpBottomMenu();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        bottomMenu.onSaveInstanceState(outState);
+    }
+
+    private void setUpBottomMenu() {
+        bottomMenu.addSpaceItem(new SpaceItem("MESSAGES", R.drawable.ic_question_answer_black_24dp));
+        bottomMenu.addSpaceItem(new SpaceItem("MAP", R.drawable.ic_map_black_24dp));
+        bottomMenu.setCentreButtonIconColorFilterEnabled(false);
+        bottomMenu.setSpaceOnClickListener(new SpaceOnClickListener() {
+            @Override
+            public void onCentreButtonClick() {
+            }
+
+            @Override
+            public void onItemClick(int itemIndex, String itemName) {
+            }
+
+            @Override
+            public void onItemReselected(int itemIndex, String itemName) {
+            }
+        });
     }
 
     @Override
@@ -53,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
                                 Toast.makeText(MainActivity.this, R.string.name_cant_be_empty, Toast.LENGTH_SHORT).show();
                             }else {
                                 title.setText(newName.getText().toString());
-                                Toast.makeText(MainActivity.this, R.string.name_saved, Toast.LENGTH_SHORT).show();
+                                Toast.makeText(MainActivity.this, R.string.name_changed_to + newName.getText().toString(), Toast.LENGTH_SHORT).show();
                                 dialog.dismiss();
                             }
                         });
