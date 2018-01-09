15d19bd47b1c6c12b68a33c18eca58b666f92158
ledsoon <ledek89@gmail.com>
2018-01-09 01:18:52 +0100

    Messages are taken from the server, but not viewed yet

--- app/src/main/java/com/example/ledsoon/ghostssip/MessagesListFragment.java
+++ app/src/main/java/com/example/ledsoon/ghostssip/MessagesListFragment.java
@@ -1,13 +1,33 @@
 package com.example.ledsoon.ghostssip;
 
 
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.android.volley.ParseError;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,7 +35,10 @@ import java.util.List;
 
 public class MessagesListFragment extends Fragment {
 
+    private final String serverBaseURL = "http://192.168.0.175";
     private RecyclerView messagesListRecyclerView;
+    private JSONArray listOfMessages;
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
     public MessagesListFragment() {
     }
@@ -31,14 +54,39 @@ public class MessagesListFragment extends Fragment {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         messagesListRecyclerView.setLayoutManager(linearLayoutManager);
+        getListOfMessages();
 
         MessagesAdapter messagesAdapter = new MessagesAdapter(createRandomNewsList(30));
 
         messagesListRecyclerView.setAdapter(messagesAdapter);
     }
 
+    private void getListOfMessages() {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
+        if (Build.VERSION.SDK_INT >= 23 &&
+                ContextCompat.checkSelfPermission( getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission( getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(getContext(), R.string.allow_app_to_access_devices_location, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(getActivity(), location -> {
+            if (location != null) {
+                getMessagesNearLocation(location);
+            }
+        });
+
+    }
+
+    private void getMessagesNearLocation(Location location) {
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(serverBaseURL.concat("/ghostssip/get_messages.php?usersLatitude=" + location.getLatitude() + "&usersLongitude=" + location.getLongitude()),
+                response -> listOfMessages = response,
+                error -> {if(error instanceof ParseError) Toast.makeText(getContext(), R.string.no_messages_found_in_your_area, Toast.LENGTH_SHORT).show();});
+        requestQueue.add(jsonArrayRequest);
+    }
+
     private List<SingleMessage> createRandomNewsList(int size) {
-        List<SingleMessage> result = new ArrayList<SingleMessage>();
+        List<SingleMessage> result = new ArrayList<>();
         for (int i = 1; i <= size; i++) {
             SingleMessage singleMessage = new SingleMessage();
             singleMessage.author = getString(R.string.author) + i;
