8ce7677f068c5c0af52b5e173de7064a3bf7990c
ledsoon <ledek89@gmail.com>
2018-01-18 01:33:12 +0100

    Map is zooming to user location

--- app/src/main/java/com/example/ledsoon/ghostssip/MapFragment.java
+++ app/src/main/java/com/example/ledsoon/ghostssip/MapFragment.java
@@ -10,16 +10,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     private MapView mapView;
     private GoogleMap googleMap;
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
     public MapFragment() {
     }
@@ -43,6 +49,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         MapsInitializer.initialize(getContext());
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
         this.googleMap = googleMap;
         googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         if (Build.VERSION.SDK_INT >= 23 &&
@@ -51,5 +58,11 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
             return;
         }
         googleMap.setMyLocationEnabled(true);
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(getActivity(), location -> {
+            if (location != null) {
+                CameraPosition userPosition = CameraPosition.builder().target(new LatLng(location.getLatitude(), location.getLongitude())).zoom(13).build();
+                googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(userPosition));
+            }
+        });
     }
 }
